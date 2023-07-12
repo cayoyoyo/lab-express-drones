1fea6ce358bcad2ec303ea1464b16f86433d50f2
Carlos Campagnolo <ccampagnolos@gmail.com>
2023-07-12 13:18:18 +0200

    done

--- routes/drones.js
+++ routes/drones.js
@@ -1,36 +1,69 @@
 const express = require('express');
 const router = express.Router();
+const Drone = require('../models/Drone.model');
 
 // require the Drone model here
 
 router.get('/drones', (req, res, next) => {
-  // Iteration #2: List the drones
-  // ... your code here
+  Drone.find()
+    .then(drones => {
+      res.render('drones/list', { drones });
+    })
+    .catch((err) => next(err));
 });
 
-router.get('/drones/create', (req, res, next) => {
-  // Iteration #3: Add a new drone
-  // ... your code here
+// GET route to show the create drone form
+router.get('/drones/create', (req, res) => {
+  res.render('drones/create-form');
 });
 
+// POST route to save a new drone
 router.post('/drones/create', (req, res, next) => {
-  // Iteration #3: Add a new drone
-  // ... your code here
+  const { name, propellers, maxSpeed } = req.body;
+  const newDrone = { name, propellers, maxSpeed };
+
+  Drone.create(newDrone)
+    .then(() => {
+      res.redirect('/drones');
+    })
+    .catch((err) => next(err));
 });
 
-router.get('/drones/:id/edit', (req, res, next) => {
-  // Iteration #4: Update the drone
-  // ... your code here
+
+
+router.get('/drones/:id/edit', (req, res) => {
+  const { id } = req.params;
+
+  Drone.findById(id)
+    .then(drone => {
+      res.render('drones/update-form', { drone });
+    })
+    .catch((err) => next(err));
 });
 
-router.post('/drones/:id/edit', (req, res, next) => {
-  // Iteration #4: Update the drone
-  // ... your code here
+router.post('/drones/:id/edit', (req, res) => {
+  const { id } = req.params;
+  const { name, propellers, maxSpeed } = req.body;
+
+  Drone.findByIdAndUpdate(id, { name, propellers, maxSpeed })
+    .then(() => {
+      res.redirect('/drones');
+    })
+    .catch(error => {
+      console.error('Error updating drone:', error);
+      res.render('drones/update-form', { drone: { _id: id, name, propellers, maxSpeed } });
+    });
 });
+
 
 router.post('/drones/:id/delete', (req, res, next) => {
-  // Iteration #5: Delete the drone
-  // ... your code here
+  const droneId = req.params.id;
+
+  Drone.findByIdAndDelete(droneId)
+    .then(() => {
+      res.redirect('/drones');
+    })
+    .catch((err) => next(err));
 });
 
 module.exports = router;
--- seeds/drones.seed.js
+++ seeds/drones.seed.js
@@ -1,1 +1,21 @@
 // Iteration #1
+const mongoose = require('mongoose');
+const Drone = require('../models/Drone.model');
+
+mongoose.connect('mongodb://127.0.0.1:27017/lab-express-drones', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+  .then(() => {
+    const drones = [
+      { name: "Creeper XL 500", propellers: 3, maxSpeed: 12 },
+      { name: "Racer 57", propellers: 4, maxSpeed: 20 },
+      { name: "Courier 3000i", propellers: 6, maxSpeed: 18 }
+    ];
+
+    return Drone.create(drones);
+  })
+  .then(createdDrones => {
+    console.log(`${createdDrones.length} drones created.`);
+    mongoose.connection.close();
+  }).catch((err) => next(err));
